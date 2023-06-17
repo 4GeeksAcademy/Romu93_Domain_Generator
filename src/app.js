27842fbd58bddd73f2f5e4dd93be7e0496f5c4c3
Romu9319 <129613538+Romu9319@users.noreply.github.com>
2023-06-17 11:33:23 +0000

    Generador de dominios completado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function domainGenereitor(pro, adje, nou, ext) {
+    var my_domain = "";
+    my_domain += pro[Math.floor(Math.random() * pro.length)];
+    my_domain += adje[Math.floor(Math.random() * adje.length)];
+    my_domain += nou[Math.floor(Math.random() * nou.length)];
+    my_domain += ext[Math.floor(Math.random() * ext.length)];
+
+    return my_domain;
+  }
+
+  var pronoun = ["The", "Our", "I", "We", "They"];
+  var adj = ["Great", "Big", "Deep", "Fabulous", "Shimmering"];
+  var noun = ["Jogger", "Racoon", "City", "Farm", "Space"];
+  var extencion = [".com", ".net", ".us", ".io"];
+
+  var resultado = domainGenereitor(pronoun, adj, noun, extencion);
+
+  var my_domain = document.getElementById("myDomain");
+
+  my_domain.textContent = resultado;
 };
